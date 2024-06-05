1503a401bda5e158605b1c7ed54a91d41be50718
ghdpreto <ghdpreto@gmail.com>
2024-06-05 10:58:22 -0300

    primeiro commit

---
+++ src/main/java/br/com/ghdpreto/ignite_java_crud_curso/IgniteJavaCrudCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ghdpreto.ignite_java_crud_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteJavaCrudCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteJavaCrudCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ghdpreto/ignite_java_crud_curso/IgniteJavaCrudCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ghdpreto.ignite_java_crud_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteJavaCrudCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

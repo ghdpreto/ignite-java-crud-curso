eb0d4f62299922dc922cc59c1619777b332cdf74
ghdpreto <ghdpreto@gmail.com>
2024-06-12 10:56:09 -0300

    fix: validacao de campos do payload

--- src/main/java/br/com/ghdpreto/ignite_java_crud_curso/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/ghdpreto/ignite_java_crud_curso/exceptions/ExceptionHandlerController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+
 import br.com.ghdpreto.ignite_java_crud_curso.exceptions.dtos.ErroResponseDTO;
 import br.com.ghdpreto.ignite_java_crud_curso.exceptions.dtos.ErroValidacaoResponseDTO;
 import br.com.ghdpreto.ignite_java_crud_curso.modules.curso.exceptions.CursoNotFoundException;
@@ -37,9 +39,24 @@ public class ExceptionHandlerController {
 
     }
 
+    // #### TRATAMENTO DE CAMPOS DESCONHECIDOS NO DTO ####
+    @ExceptionHandler(UnrecognizedPropertyException.class)
+    public ResponseEntity<List<ErroValidacaoResponseDTO>> handleUnrecognizedPropertyException(
+            UnrecognizedPropertyException exception) {
+
+        List<ErroValidacaoResponseDTO> dto = new ArrayList<ErroValidacaoResponseDTO>();
+
+        dto.add(new ErroValidacaoResponseDTO(exception.getPropertyName(), "Propriedade errada ou desconhecida."));
+
+        return ResponseEntity
+                .badRequest()
+                .body(dto);
+    }
+
     // #### TRATAMENTO DE ERROS ####
     @ExceptionHandler(CursoNotFoundException.class)
     public ResponseEntity<ErroResponseDTO> handleCursoNotFoundException(CursoNotFoundException e) {
         return ResponseEntity.notFound().build();
     }
+
 }
